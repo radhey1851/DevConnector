5b6dc31d7aca3dccb2c00434bff2814f84821de5
Radheshyam Etikala <radhey1851@gmail.com>
2019-05-28 03:25:30 +0530

    Added Profiles and ProfileItem with actions

--- client/src/redux/actions/profile.js
+++ client/src/redux/actions/profile.js
@@ -5,8 +5,59 @@ import {
   PROFILE_ERROR,
   UPDATE_PROFILE,
   CLEAR_PROFILE,
-  ACCOUNT_DELETED
+  GET_PROFILES,
+  ACCOUNT_DELETED,
+  GET_REPOS
 } from './types';
+
+//  Get all profiles
+export const getProfiles = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+  try {
+    const res = await axios.get('/api/profile');
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//  Get Profile by Id
+export const getProfileById = userId => async dispatch => {
+  try {
+    const res = await axios.get(`/api/profile/users/${userId}`);
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//  Get Github Repos
+export const getGithubRepos = githubUserName => async dispatch => {
+  try {
+    const res = await axios.get(`/api/profile/github/${githubUserName}`);
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
 
 //  Get Current Profile
 export const getCurrentProfile = () => async dispatch => {
